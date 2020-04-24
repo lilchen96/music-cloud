bb6e34b0053be5c82738e2278055865c5d7ba111
lilchen96 <lilchen96@outlook.com>
2020-04-24 17:36:32 +0800

    404页面样式修改

--- src/views/404NotFound.vue
+++ src/views/404NotFound.vue
@@ -1,9 +1,19 @@
 <template>
-    <h1>404</h1>
+    <div class="not-found">
+        <h1>404网站飞走了</h1>
+    </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.not-found {
+    height: 100%;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
