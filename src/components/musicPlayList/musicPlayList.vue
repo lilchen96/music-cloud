62c050b2b98c12193877a9420f05dca5897b66f7
lilchen96 <lilchen96@outlook.com>
2020-04-29 17:26:40 +0800

    歌名超出省略

--- src/components/musicPlayList/musicPlayList.vue
+++ src/components/musicPlayList/musicPlayList.vue
@@ -2,9 +2,10 @@
     <div class="music-play-list">
         <div class="top">
             <div class="right" @click="playAll">
-                <img class="icon" src="" />
-                <span class="info">播放全部</span>
-                <!-- <span class="count">{{ "(共" + otherInfo.musicCount + "首)" }}</span> -->
+                <div class="icon"><img :src="playActionIcon" /></div>
+                <div class="info">
+                    <span>播放全部</span> <span class="count">{{ "(共" + musicList.length + "首)" }}</span>
+                </div>
             </div>
             <div class="left">
                 <div class="star-btn">
@@ -16,9 +17,9 @@
         <div class="content">
             <div class="list-item" v-for="(item, index) in musicList" @click="clickMusic(item.id)">
                 <div class="left">
-                    <div class="order">{{ index + 1 }}</div>
+                    <div class="order" :style="{ fontSize: index + 1 >= 100 ? '14px' : '18px' }">{{ index + 1 }}</div>
                     <div class="music-info">
-                        <div class="name">{{ item.name }}</div>
+                        <div class="name" :style="{ width: musicInfoWidth + 'px' }">{{ item.name }}</div>
                         <div class="info">{{ item.artistName + "-" + item.albumName }}</div>
                     </div>
                 </div>
@@ -34,7 +35,15 @@
 </template>
 
 <script>
+import playActionIcon from "@/assets/images/play_action_icon.png";
+
 export default {
+    data() {
+        return {
+            playActionIcon,
+            musicInfoWidth: 200
+        };
+    },
     props: {
         musicList: {
             type: Array,
@@ -49,6 +58,11 @@ export default {
                 };
             }
         }
+    },
+
+    mounted() {
+        // TODO 获取document
+        this.musicInfoWidth = document.querySelector(".music-info").offsetWidth;
     },
 
     methods: {
@@ -77,14 +91,25 @@ export default {
     .top {
         display: flex;
         justify-content: space-between;
-        font-size: 16px;
         padding: 10px 16px;
         .right {
+            display: flex;
             .icon {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                img {
+                    width: 20px;
+                    height: 20px;
+                }
             }
             .info {
-            }
-            .count {
+                margin-left: 12px;
+                font-size: 17px;
+                .count {
+                    font-size: 14px;
+                    opacity: 0.5;
+                }
             }
         }
         .left {
@@ -95,26 +120,34 @@ export default {
         }
     }
     .content {
-        padding: 10px 16px;
+        padding: 10px 0px;
+        padding-left: 6px;
+        padding-right: 16px;
         flex: 1;
         overflow-y: scroll;
         .list-item {
             display: flex;
             justify-content: space-between;
             padding: 10px 0px;
+            font-size: 17px;
             .left {
                 display: flex;
+                justify-content: space-between;
                 .order {
                     display: flex;
                     flex-direction: column;
                     justify-content: center;
-                    font-size: 16px;
+                    width: 25px;
+                    text-align: center;
                     opacity: 0.4;
                 }
                 .music-info {
-                    margin-left: 16px;
+                    flex: 1;
+                    margin-left: 6px;
                     .name {
-                        font-size: 16px;
+                        overflow: hidden;
+                        white-space: nowrap;
+                        text-overflow: ellipsis;
                     }
                     .info {
                         font-size: 12px;
