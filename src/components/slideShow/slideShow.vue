3f3d446cbb5f2e9b95a968ef8fc74d1d184a1dc3
LilChen96 <lilchen96@outlook.com>
2020-05-19 14:49:00 +0800

    loopSlideShow

--- src/components/slideShow/slideShow.vue
+++ src/components/slideShow/slideShow.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="slide-show">
         <div
-            class="image-list"
-            :class="isTransition ? 'translate-transition' : ''"
-            :style="{ transform: 'translate(-' + (active - 1) * 100 + '%,0) translateX(' + translateX + 'px)' }"
+            v-for="(item, index) in list"
+            :key="item"
+            class="image-item"
+            :class="dynamicClass(index)"
             @touchstart="touchstart"
             @touchmove="touchmove"
             @touchend="touchend"
         >
-            <div class="image-item" v-for="item in imageList">
-                <img class="image" :src="item" />
-            </div>
+            <img class="image" :src="item" />
         </div>
         <div class="guide-point"></div>
     </div>
@@ -18,26 +17,6 @@
 
 <script>
 export default {
-    data() {
-        return {
-            // 当前图片序号
-            active: 1,
-            // 当前图片内的横向偏移量(当切换图片后一定要置为0)
-            translateX: 0,
-
-            // 当前触摸点坐标
-            currentPosition: {
-                x: 0,
-                y: 0
-            },
-
-            // 开始触摸时间点 getTime ms
-            touchstartTime: 0,
-
-            // 是否需要过渡动画 touchend结算时需要过渡动画
-            isTransition: false
-        };
-    },
     props: {
         imageList: {
             type: Array,
@@ -46,73 +25,93 @@ export default {
                 "http://p1.music.126.net/qV3v_0FRCRLhNY1a_644lA==/109951164989080106.jpg",
                 "http://p1.music.126.net/gfn8202mSvEStU1vBBYyXg==/109951164989041192.jpg"
             ]
-        },
+        }
 
-        // 是否循环
-        loop: {
-            type: Boolean,
-            default: false
+        // // 是否循环
+        // loop: {
+        //     type: Boolean,
+        //     default: true
+        // }
+    },
+    data() {
+        return {
+            // 图片列表
+            list: this.imageList,
+
+            // 当前图片序号
+            active: 1
+            // 当前图片内的横向偏移量(当切换图片后一定要置为0)
+            // translateX: 0,
+
+            // 当前触摸点坐标
+            // currentPosition: {
+            //     x: 0,
+            //     y: 0
+            // },
+
+            // 开始触摸时间点 getTime ms
+            // touchstartTime: 0,
+        };
+    },
+    computed: {
+        dynamicClass() {
+            return index => {
+                // 是否需要过渡动画 left:center和left需要动画  right:center和right需要动画
+                let className = "";
+                if (index === this.active - 1) {
+                    className = "center-item translate-transition";
+                }
+                if (index === this.active - 2 || (this.active - 2 <= 0 && index === this.list.length - 1)) {
+                    if (this.direction === "left") {
+                        className = "left-item translate-transition";
+                    } else {
+                        className = "left-item";
+                    }
+                }
+                if (index === this.active || (this.active >= this.list.length && index === 0)) {
+                    if (this.direction === "right") {
+                        className = "right-item translate-transition";
+                    } else {
+                        className = "right-item";
+                    }
+                }
+                return className;
+            };
         }
     },
     methods: {
         touchstart(e) {
-            this.touchstartTime = new Date().getTime();
+            // this.touchstartTime = new Date().getTime();
             this.currentPosition = {
                 x: e.changedTouches[0].pageX,
                 y: e.changedTouches[0].pageY
             };
-            this.isTransition = false;
+            // this.isTransition = false;
         },
         touchmove(e) {
-            // let direction = "";
-            // if (e.changedTouches[0].pageX > this.currentPosition.x) {
-            //     direction = "right";
-            // } else {
-            //     direction = "left";
-            // }
-            // if (
-            //     !this.loop &&
-            //     ((this.active === this.imageList.length && direction === "left") ||
-            //         (this.active === 1 && direction === "right"))
-            // ) {
-            //     }
-            this.translateX += e.changedTouches[0].pageX - this.currentPosition.x;
-            this.currentPosition = {
-                x: e.changedTouches[0].pageX,
-                y: e.changedTouches[0].pageY
-            };
+            // this.translateX += e.changedTouches[0].pageX - this.currentPosition.x;
+            // this.currentPosition = {
+            //     x: e.changedTouches[0].pageX,
+            //     y: e.changedTouches[0].pageY
+            // };
         },
-        touchend() {
-            const touchTime = new Date().getTime() - this.touchstartTime;
-            this.isTransition = true;
-            const width = document.querySelector(".image-list").clientWidth;
-            // 判断滑动方向
-            let direction = "";
-            if (this.translateX < -10) {
-                direction = "left";
-            }
-            if (this.translateX > 10) {
-                direction = "right";
-            }
-
-            // 滑动速度快 直接切换图片 速度慢 过50%切换 不过50%不切换
-            if (touchTime <= 200 || Math.abs(this.translateX) > width * 0.5) {
-                if (direction === "left") {
-                    if (this.active === this.imageList.length) {
-                        this.active = this.loop ? 1 : this.imageList.length;
-                    } else {
-                        this.active += 1;
-                    }
-                }
-                if (direction === "right") {
-                    if (this.active === 1) {
-                        this.active = this.loop ? this.imageList.length : 1;
-                    } else {
-                        this.active -= 1;
-                    }
+        touchend(e) {
+            if (e.changedTouches[0].pageX - this.currentPosition.x > 10) {
+                this.direction = "right";
+                if (this.active <= 1) {
+                    this.active = this.list.length;
+                } else {
+                    this.active -= 1;
                 }
             }
-            this.translateX = 0;
+            if (e.changedTouches[0].pageX - this.currentPosition.x < -10) {
+                this.direction = "left";
+                if (this.active >= this.list.length) {
+                    this.active = 1;
+                } else {
+                    this.active += 1;
+                }
+            }
         }
     }
 };
@@ -120,16 +119,23 @@ export default {
 
 <style lang="less" scoped>
 .slide-show {
-    .image-list {
-        white-space: nowrap;
-        .image-item {
-            display: inline-block;
-            width: 100%;
-            text-align: center;
-            .image {
-                width: calc(100% - 32px);
-            }
+    position: relative;
+    .image-item {
+        position: absolute;
+        width: 100%;
+        text-align: center;
+        .image {
+            width: calc(100% - 32px);
         }
+    }
+    .left-item {
+        transform: translateX(-100%);
+    }
+    .center-item {
+        transform: translateX(0);
+    }
+    .right-item {
+        transform: translateX(100%);
     }
     .translate-transition {
         transition: transform 0.5s;
---
+++ src/components/slideShow/slideShow_noloop.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="slide-show">
+        <div
+            class="image-list"
+            :class="isTransition ? 'translate-transition' : ''"
+            :style="{ transform: 'translate(-' + (active - 1) * 100 + '%,0) translateX(' + translateX + 'px)' }"
+            @touchstart="touchstart"
+            @touchmove="touchmove"
+            @touchend="touchend"
+        >
+            <div class="image-item" v-for="item in list">
+                <img class="image" :src="item" />
+            </div>
+        </div>
+        <div class="guide-point"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // 图片列表
+            list: this.imageList,
+            // 当前图片序号
+            active: 1,
+            // 当前图片内的横向偏移量(当切换图片后一定要置为0)
+            translateX: 0,
+
+            // 当前触摸点坐标
+            currentPosition: {
+                x: 0,
+                y: 0
+            },
+
+            // 开始触摸时间点 getTime ms
+            touchstartTime: 0,
+
+            // 是否需要过渡动画 touchend结算时需要过渡动画
+            isTransition: false
+        };
+    },
+    props: {
+        imageList: {
+            type: Array,
+            default: [
+                "http://p1.music.126.net/SAUL6U5oQM715WGye79VLQ==/109951164988980681.jpg",
+                "http://p1.music.126.net/qV3v_0FRCRLhNY1a_644lA==/109951164989080106.jpg",
+                "http://p1.music.126.net/gfn8202mSvEStU1vBBYyXg==/109951164989041192.jpg"
+            ]
+        }
+
+        // // 是否循环
+        // loop: {
+        //     type: Boolean,
+        //     default: true
+        // }
+    },
+    methods: {
+        touchstart(e) {
+            this.touchstartTime = new Date().getTime();
+            this.currentPosition = {
+                x: e.changedTouches[0].pageX,
+                y: e.changedTouches[0].pageY
+            };
+            this.isTransition = false;
+        },
+        touchmove(e) {
+            this.translateX += e.changedTouches[0].pageX - this.currentPosition.x;
+            this.currentPosition = {
+                x: e.changedTouches[0].pageX,
+                y: e.changedTouches[0].pageY
+            };
+        },
+        touchend() {
+            const touchTime = new Date().getTime() - this.touchstartTime;
+            this.isTransition = true;
+            const width = document.querySelector(".image-list").clientWidth;
+            // 判断滑动方向
+            let direction = "";
+            if (this.translateX < -10) {
+                direction = "left";
+            }
+            if (this.translateX > 10) {
+                direction = "right";
+            }
+
+            // 滑动速度快 直接切换图片 速度慢 过50%切换 不过50%不切换
+            if (touchTime <= 200 || Math.abs(this.translateX) > width * 0.5) {
+                if (direction === "left") {
+                    if (this.active === this.list.length) {
+                        this.active = this.list.length;
+                    } else {
+                        this.active += 1;
+                    }
+                }
+                if (direction === "right") {
+                    if (this.active === 1) {
+                        this.active = 1;
+                    } else {
+                        this.active -= 1;
+                    }
+                }
+            }
+            this.translateX = 0;
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.slide-show {
+    .image-list {
+        white-space: nowrap;
+        .image-item {
+            display: inline-block;
+            width: 100%;
+            text-align: center;
+            .image {
+                width: calc(100% - 32px);
+            }
+        }
+    }
+    .translate-transition {
+        transition: transform 0.5s;
+    }
+}
+</style>
